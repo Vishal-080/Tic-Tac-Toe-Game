4fa68f889bfe3caea51b46668c6b8fa62ee2b5b6
Vishal Rathod <vrathod591@gmail.com>
2024-01-18 01:40:13 +0530

    win func working

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -26,7 +26,39 @@ const TicTacToe = () => {
       data[num]="O";
       setCount(++count);
     }
+    checkWin();
 
+  }
+
+  const checkWin = () => {
+    if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
+      won(data[2]);
+    }
+    else if(data[3]===data[4] && data[4]===data[5] && data[5]!==""){
+      won(data[5]);
+    }
+    else if(data[6]===data[7] && data[7]===data[8] && data[8]!==""){
+      won(data[8]);
+    }
+    else if(data[0]===data[3] && data[3]===data[6] && data[6]!==""){
+      won(data[6]);
+    }
+    else if(data[1]===data[4] && data[4]===data[7] && data[7]!==""){
+      won(data[7]);
+    }
+    else if(data[2]===data[5] && data[5]===data[8] && data[8]!==""){
+      won(data[8]);
+    }
+    else if(data[0]===data[4] && data[4]===data[8] && data[8]!==""){
+      won(data[8]);
+    }
+    else if(data[2]===data[4] && data[4]===data[6] && data[6]!==""){
+      won(data[6]);
+    }
+  }
+
+  const won = (winner) => {
+    setLock(true);
   }
 
   return (
